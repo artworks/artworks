66a64fd4246f09b73ddff8f47c97290644741b23
belaz <mbelazar@gmail.com>
2010-10-27 15:13:16 +0200

    some profile lib to begin

---
+++ apps/profile/modules/myprofile/actions/actions.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * myprofile actions.
+ *
+ * @package    artworks
+ * @subpackage myprofile
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class myprofileActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+   // $this->forward('default', 'module');
+   $this->form = new CustomersForm();
+  }
+}
